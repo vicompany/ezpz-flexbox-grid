252c00a8bafd369bab7811f5812a87d99e4e2c69
Sven Sigmond <sven.sigmond@gmail.com>
2017-04-18 14:38:40 +0200

    Reworked demo pages to use partial templates

---
+++ src/docs/build.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const globby = require('globby');
+const path = require('path');
+
+const PATTERN_TAG = /\{\{(.+)\}\}/g;
+
+const getNavigationStateReplacer = (variableName) => (match, variable) => {
+	if (variable !== variableName) return '';
+	return 'is-active';
+};
+
+const buildPage = (file) => {
+	const content = fs.readFileSync(file);
+	const newContent = content.toString().replace(PATTERN_TAG, (match, filePath) => {
+		const fileName = path.basename(file, '.html');
+		const variableName = `${fileName}-state`;
+		let partialContents = fs.readFileSync(`${__dirname}/${filePath}.html`).toString();
+
+		if (filePath.includes('nav')) {
+			partialContents = partialContents
+				.replace(PATTERN_TAG, getNavigationStateReplacer(variableName));
+		}
+
+		return partialContents;
+	});
+
+	const newFileName = path.basename(file);
+
+	fs.writeFileSync(`${__dirname}/../../docs/${newFileName}`, newContent);
+};
+
+globby([`${__dirname}/*.html`])
+	.then((files) => {
+		files.forEach(buildPage);
+	}
+);
